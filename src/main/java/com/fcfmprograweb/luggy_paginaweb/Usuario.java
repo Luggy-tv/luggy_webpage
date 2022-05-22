627433a3d55851d21fffdd3209abc852c82481d6
Luis Alfonso Martinez Martinez <luisalfonsomartinezmartinez@outlook.es>
2022-05-22 14:21:06 -0500

    Añadiendo Usuario

---
+++ src/main/java/com/fcfmprograweb/luggy_paginaweb/Usuario.java
@@ -0,0 +1,18 @@
+
+package com.fcfmprograweb.luggy_paginaweb;
+import java.sql.Blob;
+
+/**
+ *
+ * @author luisa
+ */
+public class Usuario {
+    public int id;
+    public String usuario;
+    public String contraseña;
+    public String nombres;
+    public String apellidos;
+    public String mail;
+    public String fechaNac;
+    public Blob profilePic;
+}
